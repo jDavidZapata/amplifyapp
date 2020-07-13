9ffd94552fab2676d0b428b93bfebb7503711f42
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-07-13 01:23:06 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Hello World from <code>AWS</code> this is David.
-        </p>
+        </h1>
+        <p>We now have Auth!</p>
         <a
           className="App-link"
           href="https://github.com/jDavidZapata"
@@ -19,8 +21,9 @@ function App() {
           To My GitHub
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
